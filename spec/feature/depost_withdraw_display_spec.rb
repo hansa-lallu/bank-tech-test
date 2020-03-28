64fd0b9e3f2141f7ff6ed4c03a9eb263bda8ef08
hansa-lallu <hansalallu@gmail.com>
2020-03-28 17:56:39 +0000

    fixed feature tests

--- spec/feature/depost_withdraw_display_spec.rb
+++ spec/feature/depost_withdraw_display_spec.rb
@@ -7,18 +7,13 @@ describe 'Bank account' do
   let(:account) { Account.new }
   let(:statement) {  Statement.new(account) }
 
-  it 'will increase balance by 1000 if a deposit of 1000 is made' do 
-    expect(account.deposit(1000)).to eq(1000)
-  end
-
-  it 'will decrease balance by 500 if a withdraw of 500 is made after a deposit of 700' do
-    account.deposit(700)
-    expect(account.withdraw(500)).to eq(200)
-  end
-
   it 'raises an error if deposit is not a valid number' do
     expect { account.deposit('wf') }.to raise_error('Please enter a valid amount')
   end
+
+  it 'gives an empty bank statement when there has been no transactions' do 
+      expect { statement.display }.to output("date || credit || debit || balance\n").to_stdout
+  end 
 
   it "shows relevant transactions on the statement" do
     Timecop.freeze do
